5e3885bebcab6f64a89cbf1220680a9913280e70
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2023-09-18 01:08:42 +0900

    Optimize code

--- GPGFakeADB/Program.cs
+++ GPGFakeADB/Program.cs
@@ -5,16 +5,12 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 
-using GameHandle = System.Tuple<System.IntPtr, System.IntPtr>;
-
 [DllImport("user32.dll", CharSet = CharSet.Unicode)]
 static extern IntPtr FindWindow(string? lpClassName, string? lpWindowName);
-[DllImport("user32.dll", CharSet = CharSet.Unicode)]
-static extern IntPtr FindWindowEx(IntPtr hWndParent, IntPtr hWndChildAfter, string? lpClassName, string? lpWindowName);
 [DllImport("user32.dll")]
 static extern bool PrintWindow(IntPtr hWnd, IntPtr hDC, uint nFlags);
 [DllImport("user32")]
-static extern int GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+static extern int GetClientRect(IntPtr hWnd, ref RECT lpRect);
 
 [DllImport("user32.dll", CharSet = CharSet.Auto)]
 static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
@@ -27,68 +23,51 @@ const int WM_MOUSEMOVE = 0x0200;
 const int WM_LBUTTONDOWN = 0x0201;
 const int WM_LBUTTONUP = 0x0202;
 
+const int PW_CLIENTONLY = 0x01;
+
 
 const int w = 1280;
 const int h = 720;
 const string targetGame = "명일방주";
 
-static GameHandle GamePtr() {
-	var ptr = FindWindow("KIWICROSVM_1", targetGame);
-	if (ptr != IntPtr.Zero)
-		return new GameHandle(ptr, FindWindowEx(ptr, IntPtr.Zero, "subWin", "sub"));
+static IntPtr GameHandle() => FindWindow("KIWICROSVM_1", targetGame);
 
-	return new GameHandle(IntPtr.Zero, IntPtr.Zero);
+static Rectangle GameSize(IntPtr hWnd) {
+	if (hWnd == IntPtr.Zero) return Rectangle.Empty;
+
+	var rc = new RECT();
+	GetClientRect(hWnd, ref rc);
+
+	return new Rectangle(0, 0, rc.right - rc.left, rc.bottom - rc.top);
 }
 
-static Rectangle GameSize(GameHandle handle) {
-	var (ptr, sub) = handle;
-	if (ptr == IntPtr.Zero || sub == IntPtr.Zero) return Rectangle.Empty;
-
-	var outer = new RECT();
-	var inner = new RECT();
-	GetWindowRect(ptr, ref outer);
-	GetWindowRect(sub, ref inner);
-
-	var left = inner.left - outer.left;
-	var top = inner.top - outer.top;
-	var width = inner.right - inner.left;
-	var height = inner.bottom - inner.top;
-	return new Rectangle(left, top, width, height);
-}
-
-static (int, int) InterpolationPoint(int x, int y, int w1, int h1, int w2, int h2) {
-	var rx = (float)x / w1;
-	var ry = (float)y / h1;
-
-	return ((int)(rx * w2), (int)(ry * h2));
-}
+static (int, int) InterpolationPoint(int x, int y, int w1, int h1, int w2, int h2) => ((int)((float)x * w2 / w1), (int)((float)y * h2 / h1));
 
 static uint pt2LPARAM(int x, int y, int w1, int h1, int w2, int h2) {
 	var (ix, iy) = InterpolationPoint(x, y, w1, h1, w2, h2);
 	return (uint)((iy << 16) | ix);
 }
 
-static Image? Capture(GameHandle handle) {
-	var (ptr, sub) = handle;
-	if (ptr == IntPtr.Zero || sub == IntPtr.Zero) return null;
+static Image? Capture(IntPtr hWnd) {
+	if (hWnd == IntPtr.Zero) return null;
 
-	var rc = GameSize(handle);
+	var rc = GameSize(hWnd);
 
-	var bitmap = new Bitmap(w, h);
-	using var outerBitmap = new Bitmap(rc.Left + rc.Width, rc.Top + rc.Height);
-	using (var g = Graphics.FromImage(outerBitmap)) {
+	using var source = new Bitmap(rc.Left + rc.Width, rc.Top + rc.Height);
+	using (var g = Graphics.FromImage(source)) {
 		var hDC = g.GetHdc();
-		PrintWindow(ptr, hDC, 0);
+		PrintWindow(hWnd, hDC, PW_CLIENTONLY);
 		g.ReleaseHdc(hDC);
 	}
 
+	var bitmap = new Bitmap(w, h);
 	using (var g = Graphics.FromImage(bitmap)) {
 		g.InterpolationMode = InterpolationMode.Bicubic;
 		g.SmoothingMode = SmoothingMode.HighQuality;
 		g.DrawImage(
-			outerBitmap,
+			source,
 			new Rectangle(0, 0, w, h),
-			new Rectangle(rc.Left, rc.Top, rc.Width, rc.Height),
+			new Rectangle(0, 0, rc.Width, rc.Height),
 			GraphicsUnit.Pixel
 		);
 	}
@@ -96,18 +75,16 @@ static Image? Capture(GameHandle handle) {
 	return bitmap;
 }
 
-static void Click(GameHandle handle, int x, int y) {
-	var (ptr, sub) = handle;
-	if (ptr == IntPtr.Zero) return;
+static void Click(IntPtr hWnd, int x, int y) {
+	if (hWnd == IntPtr.Zero) return;
 
-	var size = GameSize(handle);
+	var size = GameSize(hWnd);
 	var lp = pt2LPARAM(x, y, w, h, size.Width, size.Height);
-	SendMessage(ptr, WM_LBUTTONDOWN, 1, lp);
-	SendMessage(ptr, WM_LBUTTONUP, 1, lp);
+	SendMessage(hWnd, WM_LBUTTONDOWN, 1, lp);
+	SendMessage(hWnd, WM_LBUTTONUP, 1, lp);
 }
-static void Swipe(GameHandle handle, int x1, int y1, int x2, int y2, int duration) {
-	var (ptr, sub) = handle;
-	if (ptr == IntPtr.Zero) return;
+static void Swipe(IntPtr hWnd, int x1, int y1, int x2, int y2, int duration) {
+	if (hWnd == IntPtr.Zero) return;
 
 	const int interval = 4;
 	var durTick = duration * TimeSpan.TicksPerSecond;
@@ -115,8 +92,8 @@ static void Swipe(GameHandle handle, int x1, int y1, int x2, int y2, int duratio
 	var begin = DateTime.Now.Ticks;
 	var end = DateTime.Now.Ticks + durTick;
 
-	var size = GameSize(handle);
-	SendMessage(ptr, WM_MOUSEMOVE, 1, pt2LPARAM(x1, y1, w, h, size.Width, size.Height));
+	var size = GameSize(hWnd);
+	SendMessage(hWnd, WM_MOUSEMOVE, 1, pt2LPARAM(x1, y1, w, h, size.Width, size.Height));
 
 	while (DateTime.Now.Ticks < end) {
 		var elapsed = DateTime.Now.Ticks - begin;
@@ -125,20 +102,20 @@ static void Swipe(GameHandle handle, int x1, int y1, int x2, int y2, int duratio
 		var x = x1 + (x2 - x1) * ratio;
 		var y = y1 + (y2 - y1) * ratio;
 		var pos = pt2LPARAM((int)x, (int)y, w, h, size.Width, size.Height);
-		SendMessage(ptr, WM_MOUSEMOVE, 1, pos);
+		SendMessage(hWnd, WM_MOUSEMOVE, 1, pos);
 
 		Thread.Sleep(interval);
 	}
 
-	SendMessage(ptr, WM_LBUTTONUP, 1, pt2LPARAM(x2, y2, w, h, size.Width, size.Height));
+	SendMessage(hWnd, WM_LBUTTONUP, 1, pt2LPARAM(x2, y2, w, h, size.Width, size.Height));
 }
 
 
-static void KeyEvent(IntPtr ptr, uint keyCode) {
-	if (ptr == IntPtr.Zero) return;
+static void KeyEvent(IntPtr hWnd, uint keyCode) {
+	if (hWnd == IntPtr.Zero) return;
 
-	SendMessage(ptr, WM_KEYDOWN, keyCode, (keyCode << 16));
-	SendMessage(ptr, WM_KEYUP, keyCode, (keyCode << 16) | 0xC0000000);
+	SendMessage(hWnd, WM_KEYDOWN, keyCode, (keyCode << 16));
+	SendMessage(hWnd, WM_KEYUP, keyCode, (keyCode << 16) | 0xC0000000);
 }
 
 static ImageCodecInfo GetEncoder(ImageFormat format) {
@@ -158,7 +135,7 @@ if (arg.Contains("connect")) {
 else if (arg.Contains("shell input tap")) {
 	var x = int.Parse(args[5]);
 	var y = int.Parse(args[6]);
-	Click(GamePtr(), x, y);
+	Click(GameHandle(), x, y);
 }
 else if (arg.Contains("shell input swipe")) {
 	var x1 = int.Parse(args[5]);
@@ -166,19 +143,18 @@ else if (arg.Contains("shell input swipe")) {
 	var x2 = int.Parse(args[7]);
 	var y2 = int.Parse(args[8]);
 	var dur = int.Parse(args[9]);
-	Swipe(GamePtr(), x1, y1, x2, y2, dur);
+	Swipe(GameHandle(), x1, y1, x2, y2, dur);
 }
 else if (arg.Contains("shell input keyevent 111")) {
-	var (ptr, _) = GamePtr();
-	KeyEvent(ptr, 0x01);
+	KeyEvent(GameHandle(), 0x01);
 }
 else if (arg.Contains("shell dumpsys window displays")) {
 	Console.WriteLine($"{w}"); // 1280 720 fixed
 	Console.WriteLine($"{h}");
 }
 else if (arg.Contains("exec-out screencap -p")) {
-	var handle = GamePtr();
-	var bitmap = Capture(handle);
+	var ptr = GameHandle();
+	var bitmap = Capture(ptr);
 
 	if (bitmap == null) return;
 
